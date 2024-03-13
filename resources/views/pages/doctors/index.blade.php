b71b1ad92f899982d458f836a4f99fd3cf522d35
ydtm <mahyudindev@gmail.com>
2024-03-13 23:59:42 +0700

    fix button edit and delet

--- resources/views/pages/doctors/index.blade.php
+++ resources/views/pages/doctors/index.blade.php
@@ -81,25 +81,7 @@
                                                 </td>
                                                 <td>{{ $doctor->created_at }}</td>
                                                 <td>
-                                                    {{-- <div class="d-flex justify-content-center">
-                                                        <a href='{{ route('doctors.edit', $doctor->id) }}'
-                                                            class="btn btn-sm btn-info btn-icon">
-                                                            <i class="fas fa-edit"></i>
-                                                            Edit
-                                                        </a>
-
-
-
-                                                        <form action="{{ route('doctors.destroy', $doctor->id) }}"
-                                                            method="POST" class="ml-2">
-                                                            <input type="hidden" name="_method" value="DELETE" />
-                                                            <input type="hidden" name="_token"
-                                                                value="{{ csrf_token() }}" />
-                                                            <button class="btn btn-danger btn-action trigger--fire-modal-1">
-                                                                <i class="fas fa-times"></i> Delete
-                                                            </button>
-                                                        </form>
-                                                    </div> --}}
+                                                    
                                                     <div class="d-flex justify-content-center">
                                                         <a href='{{ route('doctors.edit', $doctor->id) }}'
                                                             class="btn btn-primary btn-action mr-1" data-toggle="tooltip"
